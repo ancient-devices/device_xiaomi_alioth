748b069f1ff9fd8e06253d0a8629f7fd68352705
theangelofdead01 <rockinthecity142@gmail.com>
2022-02-17 00:46:25 -0500

    sm8250-common: addapt to ancient

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 PixelPlusUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/awaken_alioth.mk
+    $(LOCAL_DIR)/ancient_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    awaken_alioth-user \
-    awaken_alioth-userdebug \
-    awaken_alioth-eng
+    ancient_alioth-user \
+    ancient_alioth-userdebug \
+    ancient_alioth-eng
